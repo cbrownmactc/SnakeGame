17da5176321c4869d12898f66a5caca9b8d67024
cbrownmactc <cbrown@muskegonisd.org>
2025-01-15 14:12:24 -0500

    Added AutoRepeat option to audio

--- SnakeGame/AudioFiles.cs
+++ SnakeGame/AudioFiles.cs
@@ -13,7 +13,9 @@ namespace SnakeGame
     {
         public static readonly MediaPlayer EatFood = LoadAudio("beep.wav");
         public static readonly MediaPlayer GameOver = LoadAudio("game-over.wav");
-        private static MediaPlayer LoadAudio(string filename, bool autoReset = true)
+        private static MediaPlayer LoadAudio(string filename, 
+            bool autoReset = true,
+            bool autoRepeat = false)
         {
             MediaPlayer mp = new MediaPlayer();
             mp.Open(new Uri($"Assets/{filename}", UriKind.Relative));
@@ -23,7 +25,20 @@ namespace SnakeGame
                 mp.MediaEnded += Player_MediaEnded;
             }
 
+            if (autoRepeat)
+            {
+                mp.MediaEnded += Player_RepeatMediaEnded;
+            }
+
             return mp;
+        }
+
+        private static void Player_RepeatMediaEnded(object? sender, EventArgs e)
+        {
+            MediaPlayer player = (MediaPlayer)sender;
+            player.Stop();
+            player.Position = TimeSpan.Zero;
+            player.Play();
         }
 
         private static void Player_MediaEnded(object? sender, EventArgs e)
